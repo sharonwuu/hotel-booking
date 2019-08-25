ee8130b83d6b355f6879b1123a6b43aca4d27ca7
Sharon <sharon@Sharon-MBP.local>
2019-08-26 02:30:04 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,13 @@
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/hotel-booking/'
+    : '/'
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
 
-    // clear all existing loaders.
-    // if you don't do this, the loader below will be appended to
-    // existing loaders of the rule.
     svgRule.uses.clear()
 
-    // add replacement loader(s)
     svgRule
       .use('vue-svg-loader')
         .loader('vue-svg-loader')
