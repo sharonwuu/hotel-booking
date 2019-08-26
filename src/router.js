0e0ff9bb5ea8e51a0165a9c4c2fb5e2a14678780
Sharon <sharon@Sharon-MBP.local>
2019-08-26 22:54:38 +0800

    addHistory

--- src/router.js
+++ src/router.js
@@ -4,7 +4,7 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 export default new VueRouter({
-  // mode:'history',
+  mode:'history',
   routes: [{
       path: '/',
       name: '/home',
